079712629f5cbc61c983c2b60be1753891637d66
okeen <eneko.taberna@gmail.com>
2011-07-26 19:24:01 +0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padelotron::Application.config.secret_token = '49307d4c19f5c98823723c1b5b9ae22f0d31246e55e8699f02a97cf792861231d14522d499489830e99a387f097018f5981fe2ead19d59270b9b6e7ffde3cdf4'
