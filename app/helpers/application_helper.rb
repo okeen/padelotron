f509d4824205f34b5b022937946df14bcc3304b6
okeen <eneko.taberna@gmail.com>
2011-07-29 19:34:18 +0200

    -Added basic top navigation menu and root route pointed to 'teams#index'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def top_navigation_menu
+    content_tag :ul, :id => 'nav' do |ul|
+      (
+      "<li>#{link_to("Players", players_path)}</li>"+
+      "<li>#{link_to("Teams", teams_path)}</li>"
+        ).html_safe      
+    end
+  end
 end
+
