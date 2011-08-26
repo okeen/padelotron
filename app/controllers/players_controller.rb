b29cd953537e68daf3f2674a865779be870cdb63
Your Name <you@example.com>
2011-08-27 00:53:50 +0200

    añadindo grafico para ver as victorias de cada xogador

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -17,7 +17,7 @@ class PlayersController < ApplicationController
   # GET /players/1.xml
   def show
     @player = Player.find(params[:id])
-
+    @graph = open_flash_chart_object(600,300,"/players/:id/graph_code")
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @player }
@@ -103,5 +103,33 @@ class PlayersController < ApplicationController
       'og:description' => "#{@player.name} page at Padelotron"
     }
   end
-  
+
+  def graph_code   
+    @player = Player.find(params[:id])
+    title = Title.new("Wins - lost")
+
+    pie = Pie.new
+    pie.start_angle = 35
+    pie.animate = true
+    pie.tooltip = '#val# of #total#<br>#percent# of 100%'
+    pie.colours = ["#d01f3c", "#356aa0", "#C79810"]
+
+    contWins = 0
+    @player.player_games.each do |game|
+      if game.played_by_player(@player)
+        contWins = contWins +1
+      end
+    end
+
+    newValues = [2,3,4]
+    pie.values  = newValues
+
+    chart = OpenFlashChart.new
+    chart.title = title
+    chart.add_element(pie)
+
+    chart.x_axis = nil
+
+    render :text => chart.to_s
+  end
 end
