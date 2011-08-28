80caa9562307302248e19a4db78a05b00929bc3d
okeen <eneko.taberna@gmail.com>
2011-08-28 16:29:46 +0200

    Implemenetd YES action in playground requsts confirmation or rejection by customers

--- app/helpers/customers/playground_requests_helper.rb
+++ app/helpers/customers/playground_requests_helper.rb
@@ -1,10 +1,11 @@
 module Customers::PlaygroundRequestsHelper
 
-  def confirm_reservation_button(reservation)
-    button_to "Yes", do_confirmation_url(reservation)
+  def confirm_reservation_action_button(reservation, confirm)
+    button_to "Yes", customers_do_playground_request_path(confirm ?
+        reservation.accept_code : reservation.reject_code)
   end
 
-  def cancel_reservation_button(reservation)
+  def cancel_reservation_action_button(reservation)
     link_to "No (you will be redirected)", customer_path(current_customer)
   end
 end
