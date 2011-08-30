375f1f1b3da80c29c46dd38618a1f18a93b2c1dc
okeen <eneko.taberna@gmail.com>
2011-08-30 03:45:10 +0200

    Improved team listing layout

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -8,6 +8,16 @@ module TeamsHelper
     "".html_safe
   end
 
+  def teams_letter_filter_links_sidebar
+    content_tag :div, :id=>'catalogue_filter_letters' do
+      ('A'..'Z').to_a.collect { |letter|
+        content_tag :div, :class => 'letter_filter_letter_container clickable_container' do
+          link_to letter, teams_path
+        end
+      }.join(" ").html_safe
+    end
+  end
+
   def facebook_comments_panel(team)
     "<fb:comments href='#{team_url(team)}' num_posts='10' width='500'></fb:comments>".html_safe
   end
